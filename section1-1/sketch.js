41977c9ccb8558e063d739c35121d82a37e796cd
banquet159john <kou.tamu2003@gmail.com>
2024-10-03 22:54:56 +0900

    Change 1-1
    function, triangle, strokeweight ,fill  change

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -1,12 +1,12 @@
-function setup() {
+function setup(1,24) {
   createCanvas(128,128);
 }
 
 function draw() {
   background(0);
-  strokeWeight(4);
-  fill(178, 100, 162);
-  triangle(12,120,116,120,116,10);
+  strokeWeight(12);
+  fill(178, 120, 162);
+  triangle(19,121,117,121,140,10);
   fill(94, 185, 84);
   triangle(0,108,106,108,106,0);
   fill(255);
