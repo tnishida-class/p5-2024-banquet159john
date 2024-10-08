67d8ba9eb6ebae413fd96e28712a2527b25ac79d
banquet159john <kou.tamu2003@gmail.com>
2024-10-08 10:59:06 +0900

    Update sketch.js
    text, background, fill is changed

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -2,14 +2,14 @@ function setup()
 {createCanvas(128,128);}
 
 function draw() {
-  background(0);
+  background(120, 0, 0);
   strokeWeight(12);
-  fill(178, 120, 162);
+  fill(0, 120, 0);
   triangle(19,121,117,121,140,10);
-  fill(94, 185, 84);
+  fill(0, 0, 120);
   triangle(0,108,106,108,106,0);
   fill(255);
   textSize(32);
   textFont("serif");
-  text("46", 68, 100);
+  text("raven", 68, 100);
 }
