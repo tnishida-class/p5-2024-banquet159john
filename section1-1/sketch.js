008d1ea18b472a6d5a80a4f9df2fe5f372ad53fe
banquet159john <kou.tamu2003@gmail.com>
2024-10-08 12:08:04 +0900

    Update sketch.js
    practice

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -1,15 +1,16 @@
 function setup() 
-{createCanvas(128,128);}
+{createCanvas(200,200);}
 
 function draw() {
   background(120, 0, 0);
-  strokeWeight(12);
+  strokeWeight(4);
   fill(0, 120, 0);
-  triangle(19,121,117,121,140,10);
+  triangle(19,121,200,121,140,10);
+  strokeWeight(6);
   fill(0, 0, 120);
   triangle(0,108,106,108,106,0);
   fill(255);
-  textSize(32);
+  textSize(25);
   textFont("serif");
   text("raven", 68, 100);
 }
