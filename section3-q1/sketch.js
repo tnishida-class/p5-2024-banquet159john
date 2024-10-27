5dbf8ac730725bc389512b9336a390124c7fe006
banquet159john <kou.tamu2003@gmail.com>
2024-10-27 12:40:14 +0900

    Update sketch.js
    ensyuu

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,23 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
-
+let increment = 1 ;
+let x, y;
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 100;
+  cycle = 200;
+  x = width/2
+  y = height/2
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  let size = 50;
+  count = (count + increment) % cycle;
+  if(keyIsPressed){increment= 5;
+  }else{increment = 1;}
+  if(count < cycle/2){size = (count + cycle/2);}
+  else{size = 200 + (cycle/2 - count);}
+  ellipse(x, y, size );
 }
