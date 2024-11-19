c5eddca5dd3ab1dfc38425703656d3cc80eee389
banquet159john <kou.tamu2003@gmail.com>
2024-11-19 12:15:36 +0900

    Update sketch.js

--- section5-3/sketch.js
+++ section5-3/sketch.js
@@ -1,6 +1,6 @@
 // テキスト「関数を作る(2) 結果を戻す関数」～「総仕上げ：カレンダーを描画しよう」
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(400, 400);
   calendar(2019, 10);
 
   // isLeapYear の動作確認のため console に出力しています
@@ -16,8 +16,33 @@ function setup(){
 
 function calendar(y, m){
   let dow = dayOfWeek(y, m, 1);
-  for(let d = 1; d <= daysInMonth(y, m); d++){
-    // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
+  let days = daysInMonth(y, m);
+
+  // カレンダー描画開始位置
+  let x = 20;
+  let yPos = 40;
+  textSize(16);
+  text(y + "年 " + m + "月", x, yPos);
+  yPos += 20;
+
+  // 曜日のヘッダー
+  const daysOfWeek = ["日", "月", "火", "水", "木", "金", "土"];
+  for (let i = 0; i < daysOfWeek.length; i++) {
+    text(daysOfWeek[i], x + i * 40, yPos);
+  }
+  yPos += 20;
+
+  // カレンダーの日付描画
+  for (let i = 0; i < dow; i++) {
+    x += 40;
+  }
+  for (let d = 1; d <= days; d++) {
+    text(d, x, yPos);
+    x += 40;
+    if ((dow + d) % 7 == 0) {
+      x = 20;
+      yPos += 20;
+    }
   }
 }
 
@@ -26,7 +51,7 @@ function isLeapYear(y){
 }
 
 function daysInYear(y){
-  // BLANK[1]
+  return isLeapYear(y) ? 366 : 365;
 }
 
 function daysInMonth(y, m){
@@ -50,7 +75,27 @@ function dayOfYear(y, m, d){
 }
 
 function dayOfWeek(y, m, d){
-  // BLANK[2]
+  // 1970年1月1日を基準とする（木曜日）
+  let baseYear = 1970;
+  let baseDayOfWeek = 4; // 木曜日
+
+  // 基準日からの年単位の日数の差
+  let totalDays = 0;
+  for(let i = baseYear; i < y; i++){
+    totalDays += daysInYear(i);
+  }
+  for(let i = y; i < baseYear; i++){
+    totalDays -= daysInYear(i);
+  }
+
+  // 年内の経過日数
+  totalDays += dayOfYear(y, m, d) - 1;
+
+  // 曜日を求める
+  let dow = (baseDayOfWeek + totalDays) % 7;
+  if (dow < 0) dow += 7;
+
+  return dow;
 }
 
 function dayOfWeekAsString(dow){
