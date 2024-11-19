e8f550483edcc5700bd5422a2f0b34f23e1fb731
banquet159john <kou.tamu2003@gmail.com>
2024-11-19 12:15:30 +0900

    Update sketch.js

--- section5-2/sketch.js
+++ section5-2/sketch.js
@@ -1,12 +1,13 @@
 // テキスト「関数を作る(1) 引数のある関数」
 // 練習問題：星を描く関数を改造して正N角形を描画する関数を作ってみよう
 function setup(){
-  createCanvas(300, 100);
+  createCanvas(400, 100);
   background(200);
   fill(0);
   crossmark(10, 10, 90, 90);
   ngmark(150, 50, 80);
   star(250, 50, 40);
+  regularPolygon(4, 350, 50, 50)
 }
 
 function crossmark(x1, y1, x2, y2){
@@ -31,6 +32,18 @@ function star(cx, cy, r){
     let x = cx + cos(theta) * r;
     let y = cy + sin(theta) * r;
     vertex(x,y);
+  }
+  endShape(CLOSE);
+}
+
+function regularPolygon(n, cx, cy, r) {
+  beginShape();
+  for (let i = 0; i < n; i++) {
+    // 各頂点の角度を計算し、それに基づいて頂点の座標を求める
+    let theta = TWO_PI * i / n - HALF_PI; // -HALF_PIで上方向からスタート
+    let x = cx + sin(theta) * r;
+    let y = cy + cos(theta) * r;
+    vertex(x, y);
   }
   endShape(CLOSE);
 }
